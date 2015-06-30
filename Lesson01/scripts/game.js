ee1406c79c60c8a437a259c7f5ab797f0af17143
Andy Feng <andyfengc@gmail.com>
2015-06-30 16:08:00 -0400

    added hello world

---
+++ Lesson01/scripts/game.js
@@ -0,0 +1,18 @@
+﻿var Person = (function () {
+    function Person() {
+    }
+    Person.prototype.walks = function () {
+        console.log("hello walk");
+    };
+
+    Person.prototype.sayHello = function () {
+        console.log("how are you?");
+    };
+    return Person;
+})();
+
+function init() {
+    var john = new Person();
+    john.sayHello();
+}
+//# sourceMappingURL=game.js.map
---
+++ Lesson01/scripts/game.ts
@@ -0,0 +1,20 @@
+﻿class Person {
+    constructor() {
+
+    }
+
+    public walks() {
+        console.log("hello walk");
+    }
+
+    public sayHello() {
+        console.log("how are you?");
+    }
+}
+
+
+
+function init() {
+    var john = new Person();
+    john.sayHello();
+}
